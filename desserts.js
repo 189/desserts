0f4402751e3d095a36cefa4a83c209da3d7c8ba0
Shangwen Wang <kv.wang@gmail.com>
2017-03-21 21:12:48 +0800

    cmd

--- desserts.js
+++ desserts.js
@@ -1,61 +1,83 @@
-"use strict"
-
-const slice = Object.prototype.slice;
-
-const encode = encodeURIComponent;
-const decode = decodeURIComponent;
-
-var utils = {
-	/**
-	 * if empty json
-	 * @param  {object}  json 
-	 * @return {boolean} empty	if it doesn't has any property
-	 */
-	isEmptyObject : function(json){
-		var empty = true;
-		for(var p in json){
-			if(json.hasOwnProperty(p)){
-				empty = false;
-				break;
-			}
-		}
-		return empty;
-	},
+/**
+ * Many of these feature are simply utiliy functions.
+ * kv.wang87@gmail.com
+ * 2017-03-16 
+ */
 
 
-	/**
-	 * foreach array 
-	 * @param  {object}   collection 
-	 * @param  {function} callback   
-	 * @return {}              
-	 */
-	each : function(collection, callback) {
-	    var x, len;
-
-	    if (Array.isArray(collection)) {
-	        x = -1;
-	        len = collection.length;
-	        while (++x < len) {
-	            if (callback(collection[x], x) == false) {
-	                break;
-	            }
-	        }
-	    } else {
-	        for (x in collection) {
-	            if (collection.hasOwnProperty(x)) {
-	                callback(collection[x], x);
-	            }
-	        }
-	    }
-	},
-
-	type : function(collection){
-		var type = slice.call(collection);
-		return type.match()
+;(function(root, name, factory){
+	if(typeof module === 'object' && module.exports){
+		module.exports = factory();
 	}
-};
+	else if (typeof define == 'function'){
+		if(define.cmd){
+			define(factory);
+		}
+		else {
+			dfine(name, factory);
+		}
+	}
+	else {
+		root[name] = factory();
+	}
 
-module.exports = utils;
+})(this, 'desserts', function(){
+	"use strict"
+
+	var slice = Object.prototype.slice;
+	var encode = encodeURIComponent;
+	var decode = decodeURIComponent;	
+	
+	return {
+		/**
+		 * if empty json
+		 * @param  {object}  json 
+		 * @return {boolean} empty	if it doesn't has any property
+		 */
+		isEmptyObject : function(json){
+			var empty = true;
+			for(var p in json){
+				if(json.hasOwnProperty(p)){
+					empty = false;
+					break;
+				}
+			}
+			return empty;
+		},
+
+
+		/**
+		 * foreach array 
+		 * @param  {object}   collection 
+		 * @param  {function} callback   
+		 * @return {}              
+		 */
+		each : function(collection, callback) {
+		    var x, len;
+
+		    if (Array.isArray(collection)) {
+		        x = -1;
+		        len = collection.length;
+		        while (++x < len) {
+		            if (callback(collection[x], x) == false) {
+		                break;
+		            }
+		        }
+		    } else {
+		        for (x in collection) {
+		            if (collection.hasOwnProperty(x)) {
+		                callback(collection[x], x);
+		            }
+		        }
+		    }
+		},
+
+		type : function(collection){
+			var type = slice.call(collection);
+			// return type.match()
+		}		
+	};
+});
 
 
 
